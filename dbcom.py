0be86e07bc8956cabbb7bfb6c3ed45f3acd36d04
Gustav <gustavnore@gmail.com>
2019-05-02 15:37:44 +0200

    edit dbcomm

--- dbcom.py
+++ dbcom.py
@@ -1,5 +1,14 @@
 import dbconfig.py as cfg
 import pymysql
 
-db = pymysql.connect(cfg.mysql['host'], cfg.mysql['user'], cfg.mysql['password'], cfg.mysql['database'])
-cursor = db.cursor()
+def addBrew(brew_name, brewer, og, target_fg, target_temp):
+    db = pymysql.connect(cfg.mysql['host'], cfg.mysql['user'], cfg.mysql['password'], cfg.mysql['database']) #Open DB-connection
+    cursor = db.cursor()
+    #Creating sql query
+    sql = "INSERT INTO brew_info(name, brewer, )"
+
+
+def addMeasurement(gravity,temp):
+    db = pymysql.connect(cfg.mysql['host'], cfg.mysql['user'], cfg.mysql['password'], cfg.mysql['database']) #Open DB-connection
+    cursor = db.cursor()
+    
