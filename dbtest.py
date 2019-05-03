5323f6f25ef2b4b9d8a6b28a849a5be2c9fb2a74
Gustav <gustavnore@gmail.com>
2019-05-03 18:12:44 +0200

    test file for db comms

---
+++ dbtest.py
@@ -0,0 +1,5 @@
+import dbcom
+#dbcom.addBrew('Iaerg', 'nogu', 1.0059, 1.0009, 24)
+dbcom.addMeasurement(1, 1.0041, 24)
+#print(dbcom.getCurrentBrewId())
+#print(dbcom.getBrewInfo('BrewName',dbcom.getCurrentBrewId()))
