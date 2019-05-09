a1c7b1b0647bd32adaf162abe13dd6b5330bc2a7
Gustav <gustavnore@gmail.com>
2019-05-09 11:12:32 +0200

    Minor changes to main for improved efficiency

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ import filter
 import interface
 import RPi.GPIO as GPIO
 
+
+#Configuration paramters
+sample_time = 10  #Seconds between evry sample
+average_time = 60 #Seconds between avaraging data and uploading to DB
+no_load_samples = 30 #Number of loadcell samples
+
+
+
 #Konfigurerer interrupt ved tastetrykk
 button1=11  # Input for knapp
 button2=9   # Input for knapp
@@ -22,10 +30,6 @@ GPIO.setup(button2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.add_event_detect(button1, GPIO.RISING, callback=setmode0)
 GPIO.add_event_detect(button2, GPIO.RISING, callback=setmode1)
 
-#Configuration paramters
-sample_time = 10  #Seconds between evry sample
-average_time = 60 #Seconds between avaraging data and uploading to DB
-no_load_samples = 30 #Number of loadcell samples
 
 view=0
 
@@ -51,29 +55,48 @@ start_average_time = time.time()
 temp_samples = []
 grav_samples = []
 
+
 #Take initial spot sample
-db.addMeasurement(current_brew_id, getGravSample(), getTempSample()) #Add measurement to DB
+initial_temp = getTempSample()
+initial_grav = getGravSample()
+db.addMeasurement(current_brew_id, initial_grav, initial_temp) #Add measurement to DB
 
 
 #Main loop
 while (1):
     time_now = time.time()
 
-    #Take sample
+    #Take sample if sample-time is reached
     if (time_now - start_sample_time) > sample_time:
+        #TODO: Insert code for displaying a "busy"-screen
         temp_samples.append(getTempSample())
         grav_samples.append(getGravSample())
         start_sample_time = time_now
 
+    #Average samples and insert to DB if averaging time is reached
     if (time_now - start_average_time) > average_time:
         db.addMeasurement(current_brew_id, np.mean(grav_samples), np.mean(temp_samples)) #Add averagde of measurements from last hour to DB
         start_average_time = time_now
         temp_samples.clear()
         grav_samples.clear()
-    #Get
-    if len(temp_samples):
-        if view==0:
-            interface.disp1(temp_samples[-1], brew_name, grav_samples[-1], target_temp)
 
-        elif view==1:
+    #Update screen
+    if view==0:
+        try:
+            interface.disp1(temp_samples[-1], brew_name, grav_samples[-1], target_temp)
+        except IndexError:
+            interface.disp1(initial_temp, brew_name, initial_grav, target_temp)
+    elif view==1:
+        try:
             interface.disp2(OG, FG, grav_samples[-1])
+        except IndexError:
+            interface.disp2(OG, FG, initial_grav)
+            
+#
+#    if len(temp_samples):
+#        if view==0:
+#            interface.disp1(temp_samples[-1], brew_name, grav_samples[-1], target_temp)
+#
+#        elif view==1:
+#            interface.disp2(OG, FG, grav_samples[-1])
+#
